4b443d00e1cb9a006706bd14c263a253fdd5b991
Chandra Carney <chandracarney@me.com>
2014-11-11 14:34:13 -0700

    Implement validations on person

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,9 +1,5 @@
 require 'rails_helper'
 
-class Person < ActiveRecord::Base
-  validates :first_name, :last_name, presence: true
-end
-
 RSpec.describe Person, :type => :model do
   it 'is invalid without a first name' do
     person = Person.new(first_name: nil)
